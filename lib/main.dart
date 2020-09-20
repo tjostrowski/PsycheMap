a2804b031a0ad69afa50c1022b369ecbe4156337
tjostrowski <tjostrowski12@gmail.com>
2020-09-20 15:53:06 +0200

    main page initial design

--- lib/main.dart
+++ lib/main.dart
@@ -28,39 +28,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        elevation: 0.0,
+        centerTitle: true,    
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.white,
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Column(children: <Widget>[
+        Flexible(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
+            decoration: _boxDecoration(),
+            alignment: Alignment.center,
+          ),
+          // ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+        Flexible(
+          flex: 2,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Expanded(
+                  child: Container(
+                    decoration: _boxDecoration(),
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(20, 5, 20, 10),
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: _boxDecoration(),
+                    margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: _boxDecoration(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
+  }
+
+  BoxDecoration _boxDecoration() {
+    return BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [BoxShadow(blurRadius: 2.0, color: Colors.grey)]);
   }
 }
